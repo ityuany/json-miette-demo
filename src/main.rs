c0d243c654f35a9970ab64cd8d4290b1eff5ff30
ityuany <519495771@qq.com>
2025-04-25 16:20:13 +0800

    更新Cargo.lock以添加新依赖项，新增.vscode/settings.json以配置拼写检查，修改main.rs以实现更复杂的JSON解析功能并使用miette库进行错误报告。

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,66 @@
-use biome_json_parser::parse_json;
-use biome_json_syntax::{JsonParserOptions, JsonSyntax};
+use biome_json_parser::{JsonParserOptions, parse_json};
+use miette::{LabeledSpan, SourceSpan, miette};
 
 fn main() {
     let json = r#"
     {
-        "name":"demo"
+        "name": "@shined/doctor",
+	"version": "0.0.9",
+	"main": "index.js",
+	"types": "index.d.ts",
+	"napi": {
+		"binaryName": "doctor",
+		"packageName": "@shined/doctor",
+		"targets": [
+			"x86_64-apple-darwin",
+			"aarch64-apple-darwin",
+			"x86_64-pc-windows-msvc",
+			"aarch64-pc-windows-msvc",
+			"x86_64-unknown-linux-gnu",
+			"x86_64-unknown-linux-musl",
+			"aarch64-unknown-linux-gnu",
+			"aarch64-unknown-linux-musl"
+		]
+	},
+	"bin": "./bin/index.mjs"
     }
     "#;
 
     let parse = parse_json(json, JsonParserOptions::default());
 
-    println!("-->{:#?}", parse)
+    let root = parse.tree();
+
+    if let Some(obj_node) = root.value().unwrap().as_json_object_value() {
+        for member in obj_node.json_member_list() {
+            let member = member.ok().unwrap();
+            let name = member.name().ok().unwrap();
+            let value = member.value().ok().unwrap();
+
+            let name = name.inner_string_text().unwrap().to_string();
+
+            // let value = value.as_json_string_value().unwrap().to_string();
+            if name == "name" {
+                let n = member.value().unwrap();
+                let a = n.as_json_string_value().unwrap();
+
+                let token = a.value_token().unwrap();
+
+                let range = token.text_trimmed_range();
+
+                let start_byte: usize = range.start().into();
+                let end_byte: usize = range.end().into();
+
+                let span = SourceSpan::new(start_byte.into(), end_byte - start_byte);
+
+                let source = json.to_string();
+                let report = miette!(
+                    labels = vec![LabeledSpan::at(span, "this should be 6"),],
+                    help = "'*' has greater precedence than '+'",
+                    "Wrong answer"
+                )
+                .with_source_code(source);
+                println!("{:?}", report)
+            }
+        }
+    }
 }
